a0694a674c706c4c98833fa927146dff4f20c390
Lakshan555 <it19951836@my.sliit.lk>
2023-10-26 12:44:41 +0530

    styles config

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-
+import { CssBaseline, ThemeProvider } from "@mui/material/styles";
+import createTheme from "@mui/material";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+import { BrowserRouter, Routes } from "react-router-dom";
+import { useMemo } from "react";
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  console.log(mode);
   return (
     <div className="app">
-    
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+        </ThemeProvider>
+        <Routes>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
