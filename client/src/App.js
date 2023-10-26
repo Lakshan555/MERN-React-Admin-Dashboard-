d008869770ca94ba51c00866b5a72bd05cf768cf
Lakshan555 <it19951836@my.sliit.lk>
2023-10-26 13:30:35 +0530

    Templete and dashborad config

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import { CssBaseline, ThemeProvider } from "@mui/material/styles";
-import createTheme from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { useSelector } from "react-redux";
 import { themeSettings } from "theme";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { useMemo } from "react";
+import Dashboard from "scenes/dashboard";
+import Layout from "scenes/layout";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -17,7 +19,11 @@ function App() {
           <CssBaseline />
         </ThemeProvider>
         <Routes>
-
+          <Route element={<Layout />}>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
